f0a2b137628b322fbd00c3be0086ff352a54aecf
timbrown1776 <64847068+timbrown1776@users.noreply.github.com>
2024-04-24 16:17:52 -0400

    Initial commit/push

--- proxy/proxy.mjs
+++ proxy/proxy.mjs
@@ -6,9 +6,9 @@ await initializeAuthProxy({
     authUrl: "https://33793539.propelauthtest.com",
     integrationApiKey: "8dbd0a58e6369c541a2fc08ff973803e5c7c50555ff123bb767e7d9d7e7c227bf5376559d7687a62806c9846b6e90be8",
     proxyPort: 8000,
-    urlWhereYourProxyIsRunning: 'https://app-pc-hospice-service-rzp4s3nasq-ue.a.run.app',
+    urlWhereYourProxyIsRunning: 'https://proxy-auth-service-rzp4s3nasq-ue.a.run.app',
     target: {
-        host: 'https://proxy-auth-service-rzp4s3nasq-ue.a.run.app',
+        host: 'https://app-pc-hospice-service-rzp4s3nasq-ue.a.run.app',
         protocol: 'https:'
     },
 })
