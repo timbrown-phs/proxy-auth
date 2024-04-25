aa6a6ea9e5085e87e3101d979abd8ec8d981e799
timbrown1776 <64847068+timbrown1776@users.noreply.github.com>
2024-04-25 13:57:26 -0400

    Initial commit/push

--- proxy/proxy.mjs
+++ proxy/proxy.mjs
@@ -1,4 +1,3 @@
-
 import { initializeAuthProxy } from '@propelauth/auth-proxy'
 
 // Replace with your configuration
@@ -6,10 +5,12 @@ await initializeAuthProxy({
     authUrl: "https://33793539.propelauthtest.com",
     integrationApiKey: "8dbd0a58e6369c541a2fc08ff973803e5c7c50555ff123bb767e7d9d7e7c227bf5376559d7687a62806c9846b6e90be8",
     proxyPort: 8000,
-    urlWhereYourProxyIsRunning: 'https://proxy-auth-service-rzp4s3nasq-ue.a.run.app',
+    urlWhereYourProxyIsRunning: 'http://localhost:8000',
     target: {
-        host: 'https://app-pc-hospice-service-rzp4s3nasq-ue.a.run.app',
-        protocol: 'https:'
+        host: 'localhost',
+        port: 8501,
+        protocol: 'http:'
     },
 })
+
 
