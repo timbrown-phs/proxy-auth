e85b5e67b9f6bcbd1bfa56850099c30111aa0753
timbrown1776 <64847068+timbrown1776@users.noreply.github.com>
2024-04-29 15:57:18 -0400

    Initial commit/push

--- proxy/proxy_cloud.mjs
+++ proxy/proxy_cloud.mjs
@@ -5,7 +5,7 @@ await initializeAuthProxy({
     authUrl: "https://33793539.propelauthtest.com",
     integrationApiKey: "8dbd0a58e6369c541a2fc08ff973803e5c7c50555ff123bb767e7d9d7e7c227bf5376559d7687a62806c9846b6e90be8",
     proxyPort: 8000,
-    urlWhereYourProxyIsRunning: 'https://proxy-auth.predictivehealthsolutions.co',
+    urlWhereYourProxyIsRunning: 'https://pc-hospice.predictivehealthsolutions.co',
     target: {
         host: 'app-pc-hospice.predictivehealthsolutions.co',
         protocol: 'https:'
