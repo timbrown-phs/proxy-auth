b56dc85e98b4a895f7b7afef69e444877dc21dd6
timbrown1776 <64847068+timbrown1776@users.noreply.github.com>
2024-05-03 14:22:20 -0400

    Initial commit/push

--- proxy/proxy.mjs
+++ proxy/proxy.mjs
@@ -2,8 +2,8 @@ import { initializeAuthProxy } from '@propelauth/auth-proxy'
 
 // Replace with your configuration
 await initializeAuthProxy({
-    authUrl: "https://auth.predictivehealthsolutions.co",
-    integrationApiKey: "b62cc0767b4c82a66ed82ad941b14f19499b84384d14d16952420c70f35ea597b0eef010882f4a657a73090b563a4bcd",
+    authUrl: "https://33793539.propelauthtest.com",
+    integrationApiKey: "8dbd0a58e6369c541a2fc08ff973803e5c7c50555ff123bb767e7d9d7e7c227bf5376559d7687a62806c9846b6e90be8",
     proxyPort: 8000,
     urlWhereYourProxyIsRunning: 'https://proxy-auth.predictivehealthsolutions.co',
     target: {
@@ -11,4 +11,5 @@ await initializeAuthProxy({
         protocol: 'https:'
     },
 })
+
 
